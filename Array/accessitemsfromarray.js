bb80d742ad5be374f523b94c817412384ed195e1
sneha12199 <khoklesneha5@gmail.com>
2023-07-03 21:49:14 +0530

    okie

---
+++ Array/accessitemsfromarray.js
@@ -0,0 +1,11 @@
+let enames = ["modiji","sonia","alia","radhika"]
+//index        0         1       2       3
+console.log(enames)
+console.log(enames[0])
+console.log(enames[1])
+console.log(enames[2])
+console.log(enames[3])
+console.log(enames[4])
+
+
+
--- Array/createarray.js
+++ Array/createarray.js
@@ -1,1 +1,9 @@
-let array=[]
+// let array=[1,2,4,4,6,"sneha"]
+
+
+// console.log(array)
+
+
+let user =[{id:101, name:"sneha"},
+{id:102, name:"anand"}]
+console.log(user);
